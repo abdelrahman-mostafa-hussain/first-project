ea1dbeb87672713bb6b1f38177f71d9c9084938b
abdelrahman <eng.abdelrahman.mostafa.hussain@gmail.com>
2023-11-24 19:05:16 +0200

    the main content that roll dice

--- Dice-Rolling-Simulator.py
+++ Dice-Rolling-Simulator.py
@@ -4,7 +4,14 @@ def rollDice():
     return random.choice(range(1,6))
 
 def main():
-    print("Dice-Rolling-Simulator")
+    print("welcome to roll dise simulator")
+    while True:
+        roll=input('Press enter to roll the dice (if you want quit press q): ')
+        if roll=="q":
+            break
+        showResult=rollDice()
+        print("the roll is",showResult)
+    print("thank you for pick roll dise simulator")
 
 if __name__=="__main__":
     main()
